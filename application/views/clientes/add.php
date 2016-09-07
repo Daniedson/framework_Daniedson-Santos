bbe18fe9f076642522b9e56b52785b4a9d192076
Daniedson Medeira dos Santos <daniedson.m.santos@gmail.com>
2016-09-07 17:52:39 -0300

    Modificando Add's da Classes Views 2

--- application/views/clientes/add.php
+++ application/views/clientes/add.php
@@ -5,10 +5,26 @@
   <form action="<?=base_url(clientes/add)?>" method="post">
   	<div class="row">
   	  <div class="form-group col-md-4">
-  	  	<label for="exampleInputEmail1">Campo Um</label>
-  	  	<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+  	  	<label for="exampleInputEmail1">Nome do Cliente</label>
+  	  	<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o nome">
           </div>
-	</div>
+  	  <div class="form-group col-md-4">
+  	  	<label for="exampleInputEmail1">CPF</label>
+  	  	<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o CPF">
+          </div>
+  	  <div class="form-group col-md-4">
+  	  	<label for="exampleInputEmail1">Endereço</label>
+  	  	<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o endereço">
+          </div>
+  	  <div class="form-group col-md-4">
+  	  	<label for="exampleInputEmail1">Bairro</label>
+  	  	<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o Bairro">
+          </div>
+  	  <div class="form-group col-md-4">
+  	  	<label for="exampleInputEmail1">Número de Telefone</label>
+  	  	<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o Telefone">
+          </div>
+        </div>
 	<hr />
 
 	<div class="row">
